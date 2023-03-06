df6daa52debcfe4b4e3fb947c8e574df7a86382c
MahiBhagi <114303660+MahiBhagi@users.noreply.github.com>
2023-03-06 12:36:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ class HelloWorld
 {
  public static void main(String args[])
  {
-    System.out.println("Daily practice makes  !!!");
+    System.out.println("Daily practice makes perfect   !!!");
  }
 
 }
